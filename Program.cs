53660f65071049b09cacc9ee19947a8afce6cefc
sita-Ax <sita81@hotmail.com>
2022-09-06 11:28:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace portraitLandscape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your username ");
+            string userName = Console.ReadLine();
+            Console.WriteLine(userName + " put in a width of an image!");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(userName + " put in a height of an image!");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+
+            if (width >= height)
+            {
+                Console.WriteLine("This is a Landscape!");
+            }
+            else
+            {
+                Console.WriteLine("This is a Portrait!");
+            }
+
+        }
+    }
+}
